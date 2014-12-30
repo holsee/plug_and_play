b1d2bd1b901edd270cfd0111380ef8dac49ea199
holsee <sholdsworth@gmail.com>
2014-12-30 01:05:41 +0000

    hello world plug with integration tests

---
+++ README.md
@@ -0,0 +1,9 @@
+PlugPlayground
+==============
+
+A little project to hold plug middle-ware experiments.
+
+
+### Hello World Plug 
+
+Simple plug from the plug docs but unit and integration tests.
---
+++ test/integration/hello_plug_integration_test.exs
@@ -0,0 +1,23 @@
+defmodule Integration.HelloPlugIntegrationTest do
+  use ExUnit.Case
+
+  setup do
+    Plug.Adapters.Cowboy.http HelloPlug, []
+    :ok
+  end
+
+  test "fetching root gets hello world" do
+    body = fetch('/')
+    assert body == 'Hello world'
+  end
+
+  defp fetch(url) do
+    base_url = 'http://localhost:4000'
+
+    { :ok, {{_version, 200, _reason_phrase}, _headers, body}}
+      = :httpc.request(base_url ++ url)
+  
+    body
+  end
+
+end
